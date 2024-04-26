6bbc79a0b95b0d7cc9d922cb291072fe6b13fb08
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-04-26 11:56:33 +0200

    move to debug cause I cba fixing this

--- Automaton/Features/UI/AutoAdjustRetainerListings.cs
+++ Automaton/Features/UI/AutoAdjustRetainerListings.cs
@@ -25,6 +25,7 @@ public partial class AutoAdjustRetainerListings : Feature
     public override string Name => "Auto Adjust Retainer Listings";
     public override string Description => "Adjusts your retainers' items upon opening listings. Interrupt with Shift.";
     public override FeatureType FeatureType => FeatureType.UI;
+    public override bool isDebug => true;
 
     public bool Initialized { get; set; }
 
