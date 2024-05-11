29d59906c8140f348e69bf4bf7c7be2d879b96cf
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-05-11 21:34:37 +0200

    Update Test.cs

--- Automaton/Features/Test.cs
+++ Automaton/Features/Test.cs
@@ -24,28 +24,7 @@ public class Test : Tweak
         Svc.Framework.Update -= OnUpdate;
     }
 
-    bool IsLButtonPressed = false;
     private unsafe void OnUpdate(IFramework framework)
     {
-        if (!Player.Available || Player.Occupied) return;
-        if (!Framework.Instance()->WindowInactive && GenericHelpers.IsKeyPressed([LimitedKeys.LeftControlKey, LimitedKeys.RightControlKey]))
-        {
-            var pos = ImGui.GetMousePos();
-            if (Svc.GameGui.ScreenToWorld(pos, out var res))
-            {
-                if (GenericHelpers.IsKeyPressed(LimitedKeys.LeftMouseButton))
-                {
-                    if (!IsLButtonPressed)
-                    {
-                        Player.GameObject->SetPosition(res.X, res.Y, res.Z);
-                    }
-                    IsLButtonPressed = true;
-                }
-                else
-                {
-                    IsLButtonPressed = false;
-                }
-            }
-        }
     }
 }
